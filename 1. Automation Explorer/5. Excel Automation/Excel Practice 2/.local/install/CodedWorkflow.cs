61732acde796107fc38c846d07e1933387faaadf
unknown <Politi@INTXL22010215.koncern.local>
2023-12-19 13:28:12 +0100

    Last exercise of Automation Explorer still needed

---
+++ 1. Automation Explorer/5. Excel Automation/Excel Practice 2/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace Explorer_Excel_Practice_1_Workbook_DataTable_Activities
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
