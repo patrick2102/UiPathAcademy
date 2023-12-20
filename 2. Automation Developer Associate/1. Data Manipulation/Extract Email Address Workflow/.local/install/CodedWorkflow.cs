f3f2a840a5d207ac6fbc414dda62e317ba94d8d5
unknown <Politi@INTXL22010215.koncern.local>
2023-12-20 15:08:17 +0100

    Done with Automation Explorer

---
+++ 2. Automation Developer Associate/1. Data Manipulation/Extract Email Address Workflow/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace Practice_7_ExtractEmailAddressWorkflow
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
