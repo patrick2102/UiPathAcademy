b4f3774afd9cb9a5cb85e0b95b797d1e661bcdce
unknown <Politi@INTXL22010215.koncern.local>
2024-01-04 08:51:02 +0100

    third way through associate

---
+++ 2. Automation Developer Associate/5. Selectors in Studio Deep Dive/Highlight Workflow Items on ACME/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace Practise3_Windows
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
